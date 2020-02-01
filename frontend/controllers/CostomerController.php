5b6f4cddde8c9f26743c2404e149cfe3bd8e5dd2
uraankhayayaal <uraankhayayaal@gmail.com>
2020-02-01 17:01:17 +0300

    update

---
+++ frontend/controllers/CostomerController.php
@@ -0,0 +1,24 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+
+/**
+ * Application controller
+ */
+class CostomerController extends Controller
+{
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
